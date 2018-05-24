1d2825b21987fb934b478c2629915b55eeaf8f2e
Sergi <sergiusz.kozubek@gmail.com>
2018-05-24 14:00:07 +0200

    Init enemy and player positionss. Handle player's moves on Canvas.

--- js/app.js
+++ js/app.js
@@ -23,11 +23,11 @@
 
 //Enemy in ES6 code
 
-let allEnemies=[];
-
 class Enemy {
-  constructor(){
+  constructor(x = 100, y = 100){
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
   }
 
   update(dt){
@@ -39,15 +39,15 @@ class Enemy {
   }
 }
 
-
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
 class Player {
-  constructor() {
+  constructor(x = 200, y = 405) {
     this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
   }
 
   update(dt) {
@@ -58,16 +58,43 @@ class Player {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 
-  handleInput() {
+  handleInput(key) {
+    const horizontalMove = 100;
+    const verticalMove = 85;
+    const borderLeft = 100;
+    const borderRight = 300;
+    const borderDown = 320;
+    const borderUp = 0;
 
+    //handle move up (check if up canvas border reached)
+    if (key == 'up' && this.y >= borderUp) {
+      this.y -= verticalMove;
+    }
+    //handle move down (check if down canvas border reached)
+    if (key == 'down' && this.y <= borderDown) {
+      this.y += verticalMove;
+    }
+    //handle move left (check left canvas border reached)
+    if (key == 'left' && this.x >= borderLeft) {
+      this.x -= horizontalMove;
+    }
+    //handle move right (check right canvas border reached)
+    if (key == 'right' && this.x <= borderRight) {
+      console.log(this.x)
+      this.x += horizontalMove;
+    }
   }
 }
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let enemy1 = new Enemy(100, 60);
+let enemy2 = new Enemy(100, 145);
+let enemy3 = new Enemy(100, 230);
 
+let allEnemies=[enemy1, enemy2, enemy3];
 
+let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -81,5 +108,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-let player = new Player();
